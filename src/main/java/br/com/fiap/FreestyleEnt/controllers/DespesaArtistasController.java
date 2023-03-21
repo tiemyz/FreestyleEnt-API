0a6c8979e3459face0dcb81ea139a52bc55cdd2f
tiemyz <rm95145@fiap.com.br>
2023-03-21 10:53:03 -0300

    📑 Despesa Artista | model e controller.

--- src/main/java/br/com/fiap/FreestyleEnt/controllers/DespesaArtistasController.java
+++ src/main/java/br/com/fiap/FreestyleEnt/controllers/DespesaArtistasController.java
@@ -11,7 +11,7 @@ import br.com.fiap.FreestyleEnt.models.DespesaArtistas;
 @RestController
 public class DespesaArtistasController {
 
-    @GetMapping("/api/v1/despesaArtistas")
+    @GetMapping("/api/v1/despesa_artistas")
     public DespesaArtistas index(){
         return new DespesaArtistas(1l, new BigDecimal(100), LocalDate.now(), "banda");
     } 
