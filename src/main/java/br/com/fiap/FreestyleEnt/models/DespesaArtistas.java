a2f28cf56df735d84acd92484491fd07aabda271
tiemyz <rm95145@fiap.com.br>
2023-03-21 09:12:31 -0300

    📑 Despesa Artista | model e controller.

---
+++ src/main/java/br/com/fiap/FreestyleEnt/models/DespesaArtistas.java
@@ -0,0 +1,78 @@
+package br.com.fiap.FreestyleEnt.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class DespesaArtista {
+	
+    private Long id; 
+    private BigDecimal valor; 
+    private String artista;
+    private String gravadora;
+    private String detalhes;
+    private LocalDate dataCadastro; 
+    private LocalDate dataApresentacao;
+
+	public DespesaArtista(Long id, BigDecimal valor, String artista, String gravadora, String detalhes, LocalDate dataCadastro, LocalDate dataApresentacao) {
+		this.id = id;
+		this.valor = valor;
+		this.artista = artista;
+		this.gravadora = gravadora;
+		this.detalhes = detalhes;
+		this.dataCadastro = dataCadastro;
+		this.dataApresentacao = dataApresentacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public String getArtista() {
+		return artista;
+	}
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+	public String getGravadora() {
+		return gravadora;
+	}
+	public void setGravadora(String gravadora) {
+		this.gravadora = gravadora;
+	}
+	public String getDetalhes() {
+		return detalhes;
+	}
+	public void setDetalhes(String detalhes) {
+		this.detalhes = detalhes;
+	}
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	public LocalDate getDataApresentacao() {
+		return dataApresentacao;
+	}
+	public void setDataApresentacao(LocalDate dataApresentacao) {
+		this.dataApresentacao = dataApresentacao;
+	}
+	
+	@Override
+	public String toString() {
+		return "DespesaArtista [id=" + id + ", valor=" + valor + ", artista=" + artista + ", gravadora=" + gravadora
+				+ ", detalhes=" + detalhes + ", dataCadastro=" + dataCadastro + ", dataApresentacao=" + dataApresentacao
+				+ "]";
+	} 
+
+}
+
+
