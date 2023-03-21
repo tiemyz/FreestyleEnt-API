e7b049e3fc4bf42f7937877cb8f9a6f75e2b33d7
tiemyz <rm95145@fiap.com.br>
2023-03-20 22:41:36 -0300

    🔧 maven configurado.

---
+++ src/main/java/br/com/fiap/FreestyleEnt/FreestyleEntApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FreestyleEnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreestyleEntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreestyleEntApplication.class, args);
+	}
+
+}
